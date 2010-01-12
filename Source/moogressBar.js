cb6cf5acf7c074b73c3f6e283584199ea45f500c
Arian <arian@aryweb.nl>
2010-01-12 22:27:20 +0100

    * More code cleanup * Move parent from options to arguments (because it is mandatory) * add get percentage method

--- Source/moogressBar.js
+++ Source/moogressBar.js
@@ -26,63 +26,61 @@ var MoogressBar = new Class({
                                  parent_el:   $('moogressBar'),  // Where to place?
                                  hide:        true,  // Hide on >=100%?
                                  animation:   true,  // Want smooth animation filling the bar?
-                                 animation_settings: {unit: '%', duration: 'normal'}
+                                 animation_settings: {unit: '%', duration: 'normal',property: 'width'}
                               },
                      
                      initialize: function(options){
                          this.setOptions(options);
                          this.options.parent_el.setStyle('z-index','999');
-                         
+ 
+ 						 // Set the current percentage
+						 this.percentage = this.options.percentage;
+                        
                          // Preload important Images
                          //var bgImageAsset = new Asset.image(this.options.bgImage);
                          
                          // Draw bar
                          this.theBar = new Element('div', {
-                                                       'styles': {
-                                                           'id': 'mcPercentage',
-                                                           'display': 'block',
-                                                           'width': this.options.percentage + '%',
-                                                           'height': this.options.height,
-                                                           'background-image': 'url(' + this.options.bgImage + ')'
-                                                           
-                                                           // Border Radius deactivated, because Firefox is causing drawing problems
-                                                           // 'border-radius': '5px',
-                                                           // '-webkit-border-radius': '5px',
-                                                           // '-moz-border-radius': '5px'
-                                                        }
-                                                     });
+						 	'styles': {
+								id: 'mcPercentage',
+								display: 'block',
+								width: this.options.percentage + '%',
+								height: this.options.height,
+								'background-image': 'url(' + this.options.bgImage + ')'/*,
+								// Border Radius deactivated, because Firefox is causing drawing problems
+								'border-radius': '5px',
+								'-webkit-border-radius': '5px',
+								'-moz-border-radius': '5px'*/
+							}
+						 });
                          // Will it be Animated?
                          if(this.options.animation)
-                           this.animationFX = new Fx.Morph(this.theBar, this.options.animation_settings);
+                           this.fx = new Fx.Tween(this.theBar, this.options.animation_settings);
                          
                          // Place the bar element where it belongs to
-                         this.options.parent_el.appendChild(this.theBar);
+                         this.options.parent_el.adopt(this.theBar);
                      },
                      
                      // function to modify the percentage status
                      setPercentage: function(percentage){
-                         if(this.options.animation)
-                         {
-                           this.animationFX.cancel();  // Somehow, MooTools is causing animation glitches while it is still in the previous animation so I cancel the last animation here
-                           this.animationFX.start({'width': percentage});
-                         }
-                         else
+                         if(this.fx) {
+                           this.fx.cancel();  // Somehow, MooTools is causing animation glitches while it is still in the previous animation so I cancel the last animation here
+                           this.fx.start(this.percentage,percentage);
+                         } else {
                            this.theBar.setStyle('width', percentage + '%');
-                         
-                         this.fireEvent("change", [percentage], 0);
+                         }
+						 
+                         this.fireEvent('change',percentage,0);
                          
                          // Fade out the parent element including the bar after the bar reaches 100%
-                         if(percentage >= 100)
-                         {
-                           if(this.options.animation && this.options.hide)
-                           {
+                         if(percentage >= 100){
+                           if(this.options.animation && this.options.hide) {
                              this.options.parent_el.fade('out');
-                             this.options.parent_el.setStyle('display','none');
-                           }
-                           else if(this.options.hide)
+                           } else if(this.options.hide){
                            	 this.options.parent_el.setStyle('display','none');
-
-                           this.fireEvent("finish");
+						   }
+                           this.fireEvent('finish');
                          }
+						 this.percentage = percentage;
                      }
-                 });
+});
